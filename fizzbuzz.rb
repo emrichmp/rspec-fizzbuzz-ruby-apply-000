e39bbb57b0a5ad79e2d50a81a9d736e363de5a88
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-05-11 03:37:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@
 def fizzbuzz(num)
   if (num % 3 == 0) && (num % 5 == 0)
     puts "Fizzbuzz"
-    elsif num % 3 == 0 && num 
+    elsif num % 3 == 0
     puts "Fizz"
     elsif num % 5 == 0
     puts "Buzz"
