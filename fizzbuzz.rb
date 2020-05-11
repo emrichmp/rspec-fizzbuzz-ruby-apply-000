4b1a9ff693d5f2fb500f82e25986636779c46b46
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-05-11 03:34:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num % 3 == 0 && num 
+  if (num % 3 == 0) && (num % 5 == 0)
+    puts "Fizzbuzz"
+    elsif num % 3 == 0 && num 
     puts "Fizz"
     elsif num % 5 == 0
     puts "Buzz"
-    
 end
